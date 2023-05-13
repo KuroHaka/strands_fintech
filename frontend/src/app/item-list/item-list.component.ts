983defd9d265a5effd169e7d12fd7e0b4f3340f5
BinarySandia04 <aranseraroig@gmail.com>
2023-05-13 17:29:37 +0200

    AS

--- frontend/src/app/item-list/item-list.component.ts
+++ frontend/src/app/item-list/item-list.component.ts
@@ -19,7 +19,16 @@ export class ItemListComponent {
           { productName: "Cocacola", price: 2, currency: "€", users: [], units: 2 },
           { productName: "Fanta", price: 2, currency: "€", users: [], units: 3 },
           { productName: "Bistec", price: 7, currency: "€", users: [], units: 1 },
-          { productName: "Nissin", price: 2, currency: "€", users: [], units: 1 }
+          { productName: "Nissin", price: 2, currency: "€", users: [], units: 1 },
+
+          { productName: "Cocadsacola", price: 2, currency: "€", users: [], units: 2 },
+          { productName: "Fantdasa", price: 2, currency: "€", users: [], units: 3 },
+          { productName: "Bistdasdsaec", price: 7, currency: "€", users: [], units: 1 },
+          { productName: "Nisdsadassin", price: 2, currency: "€", users: [], units: 1 },
+          { productName: "Cocadsacola", price: 2, currency: "€", users: [], units: 2 },
+          { productName: "Fantdasa", price: 2, currency: "€", users: [], units: 3 },
+          { productName: "Bistdasdsaec", price: 7, currency: "€", users: [], units: 1 },
+          { productName: "Nisdsadassin", price: 2, currency: "€", users: [], units: 1 }
       ]
 
       
