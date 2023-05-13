7fdc60c7ef3d9783e5d78912a5fe4032fbb2bebf
Eugeni Chen <zhensheng.chen@smadex.com>
2023-05-13 11:22:32 +0200

    FIRST COMMIT

---
+++ frontend/src/app/item-unit/item-unit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-unit',
+  templateUrl: './item-unit.component.html',
+  styleUrls: ['./item-unit.component.scss']
+})
+export class ItemUnitComponent {
+
+}
