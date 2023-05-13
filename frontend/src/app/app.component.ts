f7dd25895f49973ef3b1eb1540daf86b24e0e966
Eugeni Chen <zhensheng.chen@smadex.com>
2023-05-13 12:38:45 +0200

    Item unit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ItemUnitComponent } from './item-unit/item-unit.component';
+import { Product } from './item-unit/item-unit.component';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+    title = 'frontend';
+    items: Product[] = [];
+
+    ngOnInit(): void {
+        this.items = [
+            { productName: "Cocacola", price: 2, currency: "€", users: [], units: 0 }
+        ]
+    }
 }
--- frontend/src/app/item-unit/item-unit.component.ts
+++ frontend/src/app/item-unit/item-unit.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-item-unit',
-  templateUrl: './item-unit.component.html',
-  styleUrls: ['./item-unit.component.scss']
+    selector: 'app-item-unit',
+    templateUrl: './item-unit.component.html',
+    styleUrls: ['./item-unit.component.scss']
 })
 export class ItemUnitComponent {
+    @Input() productName: string = ""
+    @Input() price: number = 0
+    @Input() currency: string = ""
+    @Input() users: string[] = []
+    @Input() units: number = 0
+}
 
+export interface Product {
+    productName: string;
+    price: number;
+    currency: string;
+    users: [];
+    units: number;
 }
