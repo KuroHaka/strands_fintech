ed6f6b597de5927cd68006b4a6f5dc943bbeb0ce
BinarySandia04 <aranseraroig@gmail.com>
2023-05-14 04:16:37 +0200

    DJSAKDK

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BackendService } from './backend.service';
+import { Observable, ReplaySubject } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
@@ -15,13 +17,52 @@ export class AppComponent implements OnInit {
     constructor(private backend: BackendService) { }
 
     ngOnInit(): void {
-
+        this.backend.post("group/get", {
+            "req": {
+              "group_id": "0"
+            }
+          }, new HttpHeaders({
+            'Content-Type': 'application/json'
+          })).subscribe((data: any) => {
+      
+            
+            if(Object.keys(data.group).length != 0) this.view2 = true;
+        });
     }
+
     onFileSelect(event: Event) {
         const target = event.target as HTMLInputElement;
         if (target.files != null){
             let image = target.files[0]
-            console.log(image.name)
+            
+            this.convertFile(image).subscribe(base64 => {
+
+                this.backend.post("group/create_receipt", {
+                    "login": {
+                        "username": "userPFM11"
+                    },
+                    
+                    "req": {
+                      "img64": base64
+                    }
+                  }, new HttpHeaders({
+                    'Content-Type': 'application/json'
+                  })).subscribe((data: any) => {
+                    this.view2 = true;
+                  })
+            });
         }
+    }
+
+    convertFile(file : File) : Observable<string> {
+        const result = new ReplaySubject<string>(1);
+        const reader = new FileReader();
+        reader.readAsBinaryString(file);
+        reader.onload = (event) => result.next(btoa(event.target!.result!.toString()));
+        return result;
+    }
+
+    uploadButton(){
+
     }
 }
