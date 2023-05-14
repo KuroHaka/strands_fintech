95319513e96206ac2f771a2a39384966c25b99f4
Eugeni Chen <zhensheng.chen@smadex.com>
2023-05-14 04:34:00 +0200

    ededewdwq

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -19,50 +19,50 @@ export class AppComponent implements OnInit {
     ngOnInit(): void {
         this.backend.post("group/get", {
             "req": {
-              "group_id": "0"
+                "group_id": "0"
             }
-          }, new HttpHeaders({
+        }, new HttpHeaders({
             'Content-Type': 'application/json'
-          })).subscribe((data: any) => {
-      
-            
-            if(Object.keys(data.group).length != 0) this.view2 = true;
+        })).subscribe((data: any) => {
+
+
+            if (Object.keys(data.group).length != 0) this.view2 = true;
         });
     }
 
     onFileSelect(event: Event) {
         const target = event.target as HTMLInputElement;
-        if (target.files != null){
+        if (target.files != null) {
             let image = target.files[0]
-            
+
             this.convertFile(image).subscribe(base64 => {
 
                 this.backend.post("group/create_receipt", {
                     "login": {
                         "username": "userPFM11"
                     },
-                    
+
                     "req": {
-                      "img64": base64
+                        "img64": base64
                     }
-                  }, new HttpHeaders({
+                }, new HttpHeaders({
                     'Content-Type': 'application/json'
-                  })).subscribe((data: any) => {
+                })).subscribe((data: any) => {
+                    console.log(data)
                     this.view2 = true;
-                  })
+                })
             });
         }
     }
 
-    convertFile(file : File) : Observable<string> {
+    convertFile(file: File): Observable<string> {
         const result = new ReplaySubject<string>(1);
         const reader = new FileReader();
         reader.readAsBinaryString(file);
         reader.onload = (event) => result.next(btoa(event.target!.result!.toString()));
         return result;
     }
-
-    uploadButton(){
+    uploadButton() {
 
     }
 }
--- frontend/src/app/item-list/item-list.component.ts
+++ frontend/src/app/item-list/item-list.component.ts
@@ -2,144 +2,131 @@ import { Component } from '@angular/core';
 import { BackendService } from '../backend.service';
 import { Product } from '../item-unit/item-unit.component';
 import { User } from '../user-draggable/user-draggable.component';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { HttpHeaders, HttpParams } from '@angular/common/http';
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { HttpHeaders } from '@angular/common/http';
 import { Observable, map, startWith } from 'rxjs';
 import { FormControl } from '@angular/forms';
 
 
 @Component({
-  selector: 'app-item-list',
-  templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.scss']
+    selector: 'app-item-list',
+    templateUrl: './item-list.component.html',
+    styleUrls: ['./item-list.component.scss']
 })
 export class ItemListComponent {
-  constructor(private backend: BackendService) { }
-  users: User[] = [];
-  items: Product[] = [];
-  selected: string[] = [];
-  currentFilter: string = "";
-  filteredOptions: Observable<string[]> | undefined;
-  options: any[] = [];
-  myControl = new FormControl<string>('');
+    constructor(private backend: BackendService) { }
+    users: Record<string, User> = {};
+    items: Product[] = [];
+    selected: string[] = [];
+    currentFilter: string = "";
+    filteredOptions: Observable<string[]> | undefined;
+    options: any[] = [];
+    myControl = new FormControl<string>('');
 
-  ngOnInit() {
-    this.RefreshView();
-  }
+    ngOnInit() {
+
+        this.users = {};
+        this.items = [];
+
+        this.filteredOptions = this.myControl.valueChanges.pipe(
+            startWith(''),
+            map(name => {
+                return name ? this._filter(name as string) : this.options.slice();
+            }),
+        );
+
+        this.backend.post("group/get", {
+            "req": {
+                "group_id": "0"
+            }
+        }, new HttpHeaders({
+            'Content-Type': 'application/json'
+        })).subscribe((data: any) => {
 
 
-  RefreshView(){
+            if (Object.keys(data.group).length == 0) return;
 
-    this.users = [];
-    this.items = [];
+            for (var i = 0; i < data.group.users.length; i++) {
+                var user = data.group.users[i];
 
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(name => {
-        return name ? this._filter(name as string) : this.options.slice();
-      }),
-    );
+                this.users[user.id] = {
+                    name: user.name,
+                    firstSurname: user.firstSurname,
+                    secondSurname: user.secondSurname,
+                    id: user.id,
+                    contribution: 0,
+                }
+            }
 
-    this.backend.post("group/get", {
-      "req": {
-        "group_id": "0"
-      }
-    }, new HttpHeaders({
-      'Content-Type': 'application/json'
-    })).subscribe((data: any) => {
+            for (var i = 0; i < data.group.products.length; i++) {
+                var product = data.group.products[i];
+                this.items.push({
+                    productName: product.product_name,
+                    price: product.price,
+                    currency: product.currency,
+                    users: new Set(product.users),
+                    units: product.units,
+                    id: i,
+                });
+            }
 
-      
-      if(Object.keys(data.group).length == 0) return;
+        })
 
-      for (var i = 0; i < data.group.users.length; i++) {
-        var user = data.group.users[i];
+
+        this.backend.post("users/get_all", {
+        }, new HttpHeaders({
+            'Content-Type': 'application/json'
+        })).subscribe((data: any) => {
+            this.options = [];
+            for (var i = 0; i < data.users.length; i++) {
+                this.options.push({
+                    "name": data.users[i].name + " " + data.users[i].firstSurname,
+                    "id": data.users[i].id
+                });
+            }
+        });
+    }
+
+    setInvite(event: string) {
+        var data = JSON.parse(event);
+
+        this.backend.post("group/users/add", {
+            "req": {
+                "group_id": "0",
+                "user_id": data.id
+            }
+        }, new HttpHeaders({
+            'Content-Type': 'application/json'
+        })).subscribe((data: any) => {
+            this.users = {};
+
+            for (var i = 0; i < data.group.users.length; i++) {
+                var user = data.group.users[i];
+                this.users[user.id] = {
+                    name: user.name,
+                    firstSurname: user.firstSurname,
+                    secondSurname: user.secondSurname,
+                    id: user.id,
+                    contribution: 0
+                }
+
+            }
+        });
+    }
+
+    private _filter(name: string): string[] {
+        const filterValue = name.toLowerCase();
+        return this.options.filter(option => option.toLowerCase().includes(filterValue));
+    }
+
+    drop(event: CdkDragDrop<User[]>) {
+        var userID = this.users[event.item.element.nativeElement.id].name
+        var productID = parseInt(event.container.element.nativeElement.id);
+        this.items[productID].users.add(userID);
+
+        //update butget and prices
+        var price_person = this.items[productID].price / this.items[productID].users.size
+        var price_person = Math.round(price_person * 100 + Number.EPSILON) / 100
         
-        this.users.push({
-          name: user.name,
-          firstSurname: user.firstSurname,
-          secondSurname: user.secondSurname,
-          id: user.id,
-          contribution: 0,
-        });
-      }
-
-      for (var i = 0; i < data.group.products.length; i++) {
-        var product = data.group.products[i];
-        console.log(product.currency);
-        this.items.push({
-          productName: product.product_name,
-          price: product.price,
-          currency: product.currency,
-          users: new Set(product.users),
-          units: product.units,
-          id: i,
-        });
-      }
-
-      console.log(data);
-    })
-
-
-    this.backend.post("users/get_all", {
-    }, new HttpHeaders({
-      'Content-Type': 'application/json'
-    })).subscribe((data: any) => {
-      console.log("Allusers")
-      console.log(data);
-
-      this.options = [];
-      for(var i = 0; i < data.users.length; i++){
-        this.options.push({
-          "name": data.users[i].name + " " + data.users[i].firstSurname + " " + data.users[i].secondSurname,
-          "id": data.users[i].id
-        });
-      }
-
-      console.log(this.options);
-    });
-  }
-
-  setInvite(event: string){
-    console.log("HOLA")
-    console.log(event);
-    var data = JSON.parse(event);
-    console.log("dataoption:",data.id.toString());
-
-    this.backend.post("group/users/add", {
-      "req": {
-        "group_id": "0",
-        "user_id": data.id
-      }
-    }, new HttpHeaders({
-      'Content-Type': 'application/json'
-    })).subscribe((data: any) => {
-      this.users = [];
-
-      for (var i = 0; i < data.group.users.length; i++) {
-        var user = data.group.users[i];
-        
-        this.users.push({
-          name: user.name,
-          firstSurname: user.firstSurname,
-          secondSurname: user.secondSurname,
-          id: user.id,
-          contribution: 0,
-        });
-      }
-    });
-  }
-
-  private _filter(name: string): string[] {
-    const filterValue = name.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
-  }
-
-  drop(event: CdkDragDrop<User[]>) {
-    var userID = event.item.element.nativeElement.id
-    console.log(userID)
-    var productID = event.container.element.nativeElement.id;
-    console.log(productID)
-
-    this.items[parseInt(productID)].users.add(userID);
-  }
+    }
 }
